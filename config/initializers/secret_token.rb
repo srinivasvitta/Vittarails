9f3c2e95b779d0ca976709419da564874dcfaf82
svitta <srinivasvitta@gmail.com>
2013-05-20 12:59:13 +0100

    First Commit£

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vittarails::Application.config.secret_token = 'e53d667b202b249c7491eff28c64fb4f1b9769981144c0db02fa0750921e2ce87b064fb3bd016c86b7b2749b0b2ea54f2787614946edac7a8e1524bc45745259'
